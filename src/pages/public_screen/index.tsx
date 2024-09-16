7a3505dedc360cf4d969345fd79860de71e1e696
Calvin C Chan <hello@calvin-c.com>
2024-09-16 15:54:52 -0700

    chore: public screen placeholder

---
+++ src/pages/public_screen/index.tsx
@@ -0,0 +1,15 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const PublicScreen: React.FC = () => {
+  return (
+    <div>
+      <Typography variant="h3">Welcome to the Public Screen</Typography>
+      <Typography variant="body1">
+        This is a public screen component.
+      </Typography>
+    </div>
+  );
+};
+
+export default PublicScreen;
